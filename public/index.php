83af6d0165bad24ceb0665ab92537ab68b3759f4
ZhiFeng Hu <hufeng1987@gmail.com>
2015-07-28 14:02:45 +0000

    Add basepath

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-define('BASE_PATH', realpath(dirname(dirname(__FILE__))));
+define('BASE_PATH', realpath(dirname(__DIR__)));
 
 define('APPLICATION_PATH', BASE_PATH . '/application');
 //define('APPLICATION_ENV', 'production');

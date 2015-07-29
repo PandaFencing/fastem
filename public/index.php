c4524e14715d12f1e3e0b5c90cad5e92e3b28bf9
ZhiFeng Hu <hufeng1987@gmail.com>
2015-07-29 00:47:22 +0000

    New autoload

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 define('BASE_PATH', realpath(dirname(__DIR__)));
 chdir(dirname(__DIR__));
-require 'init_autoloader.php';
+include 'vendor/autoload.php';
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
 
 

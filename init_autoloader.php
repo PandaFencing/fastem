d18c94844c2796df6f651a70b87c0f97b604c220
ZhiFeng Hu <hufeng1987@gmail.com>
2015-08-06 08:27:23 +0800

    Add composer.phar

--- init_autoloader.php
+++ init_autoloader.php
@@ -1,6 +1,6 @@
 <?php
-Zend\Loader\AutoloaderFactory::factory(array(
-    'Zend\Loader\StandardAutoloader' => array(
-        'autoregister_zf' => true
-    )
-));
+set_error_handler(function ($severity, $message, $file, $line) {
+    error_log(((string) new ErrorException($message, 0, $severity, $file, $line)) . PHP_EOL);
+})
+include 'vendor/autoload.php';
+

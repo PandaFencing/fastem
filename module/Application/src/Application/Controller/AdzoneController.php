3a58ec19783118ffdd1fbabd13ec2d474cc7a2cb
ZhiFeng Hu <hufeng1987@gmail.com>
2015-08-26 08:11:32 +0800

    Code clean for AdzoneController
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- module/Application/src/Application/Controller/AdzoneController.php
+++ module/Application/src/Application/Controller/AdzoneController.php
@@ -6,47 +6,47 @@ use Zend\Mvc\Controller\AbstractActionController;
 
 class AdzoneController extends AbstractActionController
 {
-	private $_redirector = null;
-	private $_flashMessenger = null;
+	private $redirector = null;
+	private $flashMessenger = null;
 	public function init() {
-		$this->_redirector = $this->_helper->getHelper('Redirector');
-		$this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
+		$this->redirector = $this->_helper->getHelper('Redirector');
+		$this->flashMessenger = $this->_helper->getHelper('FlashMessenger');
 		$auth = Zend_Auth::getInstance();
 		if (!$auth->hasIdentity()) {
-			$this->_redirector->gotoSimple('index', 'login', 'auth');
+			$this->redirector->gotoSimple('index', 'login', 'auth');
 		}
 	}
 	public function indexAction() {
 		$db = Zend_Registry::get('db');
 		$adzone = $db->fetchAll('SELECT * FROM adzone;');
 		$this->view->adzone = $adzone;
-		$this->view->messages = $this->_flashMessenger->getMessages();
+		$this->view->messages = $this->flashMessenger->getMessages();
 		$this->view->server_name = $_SERVER['SERVER_NAME'];
 	}
 	public function addAction() {
-		$this->view->messages = $this->_flashMessenger->getMessages();
+		$this->view->messages = $this->flashMessenger->getMessages();
 	}
 	public function saveaddAction() {
-		if (!$this->_request->isPost()) {
-			$this->_redirector->gotoSimple('index');
+		if (!$this->getRequest()->isPost()) {
+			$this->redirector->gotoSimple('index');
 		}
 
-		$name = $this->_getParam('name');
+		$name = $this->params()->fromQuery('name');
 		if (empty($name)) {
-			$this->_flashMessenger->addMessage('广告位名不能为空');
-			$this->_redirector->gotoSimple('add');
+			$this->flashMessenger->addMessage('广告位名不能为空');
+			$this->redirector->gotoSimple('add');
 		}
-		$width = $this->_getParam('width');
+		$width = $this->params()->fromQuery('width');
 		if (empty($width) || !is_numeric($width)) {
-			$this->_flashMessenger->addMessage('广告位宽度不能为空');
-			$this->_redirector->gotoSimple('add');
+			$this->flashMessenger->addMessage('广告位宽度不能为空');
+			$this->redirector->gotoSimple('add');
 		}
-		$height = $this->_getParam('height');
+		$height = $this->params()->fromQuery('height');
 		if (empty($height) || !is_numeric($height)) {
-			$this->_flashMessenger->addMessage('广告位高度不能为空');
-			$this->_redirector->gotoSimple('add');
+			$this->flashMessenger->addMessage('广告位高度不能为空');
+			$this->redirector->gotoSimple('add');
 		}
-		$description = $this->_getParam('description');
+		$description = $this->params()->fromQuery('description');
 		$data = array(
 			'name' => $name,
 			'width' => $width,
@@ -55,49 +55,49 @@ class AdzoneController extends AbstractActionController
 		);
 		$db = Zend_Registry::get('db');
 		$db->insert('adzone', $data);
-		$this->_flashMessenger->addMessage('成功增加一条广告位');
-		$this->_redirector->gotoSimple('index');
+		$this->flashMessenger->addMessage('成功增加一条广告位');
+		$this->redirector->gotoSimple('index');
 	}
 	public function editAction() {
-		$id = intval($this->_getParam('id'));
+		$id = intval($this->params()->fromQuery('id'));
 		if (empty($id)) {
-			$this->_flashMessenger->addMessage('没有指定要编辑的广告位');
-			$this->_redirector->gotoSimple('index');
+			$this->flashMessenger->addMessage('没有指定要编辑的广告位');
+			$this->redirector->gotoSimple('index');
 		}
 		$db = Zend_Registry::get('db');
 		$adzone = $db->fetchRow('SELECT * FROM adzone WHERE id = ' . $id);
 		if (false == $adzone) {
-			$this->_flashMessenger->addMessage('找不到要编辑的广告位');
-			$this->_redirector->gotoSimple('index');
+			$this->flashMessenger->addMessage('找不到要编辑的广告位');
+			$this->redirector->gotoSimple('index');
 		}
 		$this->view->adzone = $adzone;
 	}
 	public function saveeditAction() {
-	if (!$this->_request->isPost()) {
-			$this->_redirector->gotoSimple('index');
+	if (!$this->getRequest()->isPost()) {
+			$this->redirector->gotoSimple('index');
 		}
-	    $id = $this->_getParam('id');
+	    $id = $this->params()->fromQuery('id');
 		if (empty($id)) {
-			$this->_flashMessenger->addMessage('对不起，没有指定要编辑的广告位');
-			$this->_redirector->gotoSimple('index');
+			$this->flashMessenger->addMessage('对不起，没有指定要编辑的广告位');
+			$this->redirector->gotoSimple('index');
 		}
 
-		$name = $this->_getParam('name');
+		$name = $this->params()->fromQuery('name');
 		if (empty($name)) {
-			$this->_flashMessenger->addMessage('广告位名不能为空');
-			$this->_redirector->gotoSimple('add');
+			$this->flashMessenger->addMessage('广告位名不能为空');
+			$this->redirector->gotoSimple('add');
 		}
-		$width = $this->_getParam('width');
+		$width = $this->params()->fromQuery('width');
 		if (empty($width) || !is_numeric($width)) {
-			$this->_flashMessenger->addMessage('广告位宽度不能为空');
-			$this->_redirector->gotoSimple('add');
+			$this->flashMessenger->addMessage('广告位宽度不能为空');
+			$this->redirector->gotoSimple('add');
 		}
-		$height = $this->_getParam('height');
+		$height = $this->params()->fromQuery('height');
 		if (empty($height) || !is_numeric($height)) {
-			$this->_flashMessenger->addMessage('广告位高度不能为空');
-			$this->_redirector->gotoSimple('add');
+			$this->flashMessenger->addMessage('广告位高度不能为空');
+			$this->redirector->gotoSimple('add');
 		}
-		$description = $this->_getParam('description');
+		$description = $this->params()->fromQuery('description');
 		$data = array(
 			'name' => $name,
 			'width' => $width,
@@ -106,19 +106,19 @@ class AdzoneController extends AbstractActionController
 		);
 		$db = Zend_Registry::get('db');
 		$db->update('adzone', $data, 'id = ' . $id);
-		$this->_flashMessenger->addMessage('成功保存编辑广告位');
-		$this->_redirector->gotoSimple('index');
+		$this->flashMessenger->addMessage('成功保存编辑广告位');
+		$this->redirector->gotoSimple('index');
 	}
 	public function deleteAction() {
-		$id = intval($this->_getParam('id'));
+		$id = intval($this->params()->fromQuery('id'));
 		if ($id ==0 ) {
-			$this->_flashMessenger->addMessage('ID 非法');
-			$this->_redirector->gotoSimple('index');
+			$this->flashMessenger->addMessage('ID 非法');
+			$this->redirector->gotoSimple('index');
 		} else {
 			$db = Zend_Registry::get('db');
 			$n = $db->delete('adzone', 'id = ' . $id);
-			$this->_flashMessenger->addMessage('成功删除一条广告位');
-			$this->_redirector->gotoSimple('index');
+			$this->flashMessenger->addMessage('成功删除一条广告位');
+			$this->redirector->gotoSimple('index');
 		}
 	}
 }

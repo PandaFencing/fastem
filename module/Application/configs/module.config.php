0125cb7b70f0062907ba974863e069edb5bbef21
ZhiFeng Hu <hufeng1987@gmail.com>
2015-08-29 09:25:35 +0800

    Add content for module.config.php
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- module/Application/configs/module.config.php
+++ module/Application/configs/module.config.php
@@ -1,22 +1,34 @@
-[production]
-phpSettings.display_startup_errors = 1
-phpSettings.display_errors = 1
-bootstrap.path = APPLICATION_PATH "/Bootstrap.php"
-bootstrap.class = "Bootstrap"
-appnamespace = "Application"
-resources.modules[] = 
-resources.frontController.moduleDirectory  = APPLICATION_PATH "/modules"
-resources.frontController.env = APPLICATION_ENV
-resources.db.adapter = "pdo_mysql"
-resources.db.params.host = "localhost"
-resources.db.params.username = "fastem"
-resources.db.params.password = "fastem"
-resources.db.params.dbname = "fastem"
-resources.db.params.charset = "utf8"
-resources.db.isDefaultTableAdapter = true
-
-[development : production]
-phpSettings.display_startup_errors = 1
-phpSettings.display_errors = 1
-
-
+<?php
+return [
+  'router' => [
+      'routes' => [
+          'type' => 'literal',
+          'options' => [
+              'route' => '/',
+              'defaults' => [
+                  'controller' => 'Application\Controller\Index',
+                  'action' => 'index'
+              ]
+          ]
+      ]
+  ],
+  'controllers' => [
+      'invokables' => [
+          'Application\Controller\Index' => 'Application\Controller\IndexController'
+      ]
+  ],
+  'view_manager' => [
+      'display_not_found_reason' => true,
+      'display_exceptions' => true,
+      'not_found_template' => 'error/404',
+      'exception_template' => 'error/index',
+      'template_map' => [
+          'layout/layout' => __DIR__.'/../view/layout.phtml',
+          'error/404' => __DIR__.'/../view/error/404.phtml',
+          'error/index' => __DIR__.'/../view/error/index.phtml'
+      ],
+      'template_path_stack' => [
+          __DIR__.'/../view'
+      ]
+  ]
+];

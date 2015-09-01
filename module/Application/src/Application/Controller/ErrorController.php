3e251cf25ffa32dc3493f19c13bf1cf1f0b84b12
ZhiFeng Hu <hufeng1987@gmail.com>
2015-09-01 08:31:51 +0800

    Reformat code for ErrorController
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- module/Application/src/Application/Controller/ErrorController.php
+++ module/Application/src/Application/Controller/ErrorController.php
@@ -11,24 +11,24 @@ class ErrorController extends AbstractActionController
         $errors = $this->_getParam('error_handler');
 
         switch ($errors->type) {
-        case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
-        case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
-        case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
 
-            // 404 error -- controller or action not found
-            $this->getResponse()->setHttpResponseCode(404);
-            $this->view->message = 'Page not found';
-            break;
-        default:
-            // application error
-            $this->getResponse()->setHttpResponseCode(500);
-            $this->view->message = 'Application error';
-            break;
+                // 404 error -- controller or action not found
+                $this->getResponse()->setHttpResponseCode(404);
+                $this->view->message = 'Page not found';
+                break;
+            default:
+                // application error
+                $this->getResponse()->setHttpResponseCode(500);
+                $this->view->message = 'Application error';
+                break;
         }
 
         $this->view->env = APPLICATION_ENV;
         $this->view->exception = $errors->exception;
-        $this->view->request   = $errors->request;
+        $this->view->request = $errors->request;
     }
 }
 
